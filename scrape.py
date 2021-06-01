59c57ca52a664cd297bc98162b28aecb0aaa6bd0
Nick Young <nick.young@auckland.ac.nz>
2021-06-01 16:58:49 +1200

    extract multiple pages, handle places closed on certain days

--- scrape.py
+++ scrape.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from openlocationcode import openlocationcode as olc
 from datetime import datetime
+from tqdm import tqdm
 import time
 import re
 import json
@@ -26,84 +27,102 @@ def pprint_times(times):
         print(day, times[i])
 
 features = []
-for i in range(20):
-    places = []
-    scrollCount = 0
-    while len(places) < 20 and scrollCount < 10:
-        scrollCount += 1
-        print("scrolling")
-        driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight)", driver.find_element_by_css_selector("div[aria-label='Results for place of interest']"))
-        time.sleep(.1)
-        places = driver.find_elements_by_css_selector("div[aria-label='Results for place of interest'] a[aria-label]")
-    place = places[i]
-    name = place.get_attribute('aria-label')
-    print(f"Clicking on {name}")
-    place.click()
-    link = place.get_attribute("href")
-    code = driver.find_element_by_css_selector("button[data-tooltip='Copy plus code']").text
-    approx_ll = re.search(f'(?P<lat>-?\d+\.\d+).+?(?P<lng>-?\d+\.\d+)', link).groupdict()
-    approx_lat = float(approx_ll["lat"])
-    approx_lng = float(approx_ll["lng"])
-    codeArea = olc.decode(olc.recoverNearest(code.split()[0], approx_lat, approx_lng))
-    lat = codeArea.latitudeCenter
-    lng = codeArea.longitudeCenter
-    address = None
-    try:
-        address = driver.find_element_by_css_selector("button[data-tooltip='Copy address']").get_attribute("aria-label").split(":")[-1].strip()
-    except NoSuchElementException:
-        pass
-    try:
-        popular = driver.find_element_by_css_selector("div.section-popular-times")
-        print("Has popular times")
-        times = [[0]*24 for _ in range(7)] # 2D matrix, 7 days of the week, 24h per day
-        dow = 0
-        hour_prev = 0
-        live_info = None
-        for elem in driver.find_elements_by_css_selector("div.section-popular-times-bar"):
-            bits = elem.get_attribute("aria-label").split()
-            if bits[0] == "Currently":
-                hour += 1
-                live_info = {
-                    "frequency": int(bits[1].rstrip("%")),
-                    "hour": hour,
-                    "day": days[dow % 7]
-                }
-                times[dow % 7][hour] = int(bits[-2].rstrip("%"))
-            else:
-                am_pm = bits[-1]
-                hour = int(bits[-2])
-                if hour == 12:
-                    hour = 0
-                if am_pm == "pm.":
-                    hour += 12
-                if hour < hour_prev:
-                    dow += 1
-                hour_prev = hour
-                times[dow % 7][hour] = int(bits[0].rstrip("%"))
-        #pprint_times(times)
-        feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [lng, lat]
-            },
-            "properties": {
-                "name": name,
-                "address": address,
-                "link": link,
-                "code": code,
-                "live_info": live_info,
-                "populartimes": times,
-                "scraped_at": datetime.now().isoformat(sep=" ", timespec="seconds")
-            }
-        }
-        print(feature)
-        features.append(feature)
-    except NoSuchElementException:
-        print("No popular times available, skipping")
-    driver.find_element_by_xpath('//button/span[text()="Back to results"]').click()
-driver.find_element_by_css_selector("button[aria-label=' Next page ']").click()
 
+def extract_page():
+    for i in tqdm(range(20)):
+        places = []
+        scrollCount = 0
+        while len(places) < 20 and scrollCount < 10:
+            scrollCount += 1
+            #print("scrolling")
+            driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight)", driver.find_element_by_css_selector("div[aria-label='Results for place of interest']"))
+            time.sleep(1)
+            places = driver.find_elements_by_css_selector("div[aria-label='Results for place of interest'] a[aria-label]")
+        place = places[i]
+        name = place.get_attribute('aria-label')
+        print(f"Clicking on {name}")
+        place.click()
+        link = place.get_attribute("href")
+        code = driver.find_element_by_css_selector("button[data-tooltip='Copy plus code']").text
+        approx_ll = re.search(f'(?P<lat>-?\d+\.\d+).+?(?P<lng>-?\d+\.\d+)', link).groupdict()
+        approx_lat = float(approx_ll["lat"])
+        approx_lng = float(approx_ll["lng"])
+        codeArea = olc.decode(olc.recoverNearest(code.split()[0], approx_lat, approx_lng))
+        lat = codeArea.latitudeCenter
+        lng = codeArea.longitudeCenter
+        address = None
+        try:
+            address = driver.find_element_by_css_selector("button[data-tooltip='Copy address']").get_attribute("aria-label").split(":")[-1].strip()
+        except NoSuchElementException:
+            pass
+        try:
+            popular = driver.find_element_by_css_selector("div.section-popular-times")
+            print("Has popular times")
+            times = [[0]*24 for _ in range(7)] # 2D matrix, 7 days of the week, 24h per day
+            dow = 0
+            hour_prev = 0
+            live_info = None
+            for elem in driver.find_elements_by_css_selector("div.section-popular-times-bar"):
+                bits = elem.get_attribute("aria-label").split()
+                if bits[0] == "%":
+                    # Closed on this day
+                    dow += 1
+                elif bits[0] == "Currently":
+                    hour += 1
+                    live_info = {
+                        "frequency": int(bits[1].rstrip("%")),
+                        "hour": hour,
+                        "day": days[dow % 7]
+                    }
+                    times[dow % 7][hour] = int(bits[-2].rstrip("%"))
+                else:
+                    am_pm = bits[-1]
+                    hour = int(bits[-2])
+                    if hour == 12:
+                        hour = 0
+                    if am_pm == "pm.":
+                        hour += 12
+                    if hour < hour_prev:
+                        dow += 1
+                    hour_prev = hour
+                    times[dow % 7][hour] = int(bits[0].rstrip("%"))
+            #pprint_times(times)
+            feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [lng, lat]
+                },
+                "properties": {
+                    "name": name,
+                    "address": address,
+                    "link": link,
+                    "code": code,
+                    "live_info": live_info,
+                    "populartimes": times,
+                    "scraped_at": datetime.now().isoformat(sep=" ", timespec="seconds")
+                }
+            }
+            #print(feature)
+            features.append(feature)
+        except NoSuchElementException:
+            print("No popular times available, skipping")
+        driver.find_element_by_xpath('//button/span[text()="Back to results"]').click()
+
+while True:
+    try:
+        extract_page()
+        try:
+            driver.find_element_by_css_selector("button[aria-label=' Next page ']").click()
+            print("Going to next page")
+        except NoSuchElementException:
+            print("All done!")
+            break
+    except Exception as e:
+        print(f"ERROR: {e}")
+        break
+
+print(f"Extracted {len(features)} places")
 driver.close()
 
 geojson = {
