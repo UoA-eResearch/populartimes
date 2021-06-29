13a34765bb71f08bc586beec1c0df7cd69cf8c1f
Nick Young <nick.young@auckland.ac.nz>
2021-06-30 09:25:46 +1200

    add missing >

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 git pull
 python3 update_populartimes_data.py
 python3 geojson2csv.py
-git commit -am "auto update" --author="populartimes-bot <ubuntu@elevation.auckland-cer.cloud.edu.au"
+git commit -am "auto update" --author="populartimes-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
