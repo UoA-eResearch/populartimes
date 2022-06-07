cfa850c421939767a09cd34cfb2242e4dc718965
Nick Young <nick.young@auckland.ac.nz>
2022-06-07 12:58:47 +1200

    Do a git pull before commiting in update.sh

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 git pull
 python3 update_populartimes_data.py
 python3 geojson2csv.py
+git pull
 git commit -am "auto update" --author="populartimes-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
