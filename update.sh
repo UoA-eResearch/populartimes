5887f4779c7ddb8e0c4d3baccc35c1dda3f03898
Nick Young <nick.young@auckland.ac.nz>
2023-10-26 14:58:06 +1300

    don't commit the whole folder in update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@ git pull
 python3 update_populartimes_data.py
 python3 geojson2csv.py
 git pull
-git commit -am "auto update" --author="populartimes-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
+git add data.csv data.geojson
+git commit -m "auto update" --author="populartimes-bot <ubuntu@elevation.auckland-cer.cloud.edu.au>"
 git push
